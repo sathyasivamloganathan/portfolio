31da78ce34bd13d3cbbb38801d03aa0c81827574
Sathya Sivam <sathyasivamloganathan@gmail.com>
2025-06-16 14:46:23 +0530

    Jenkinsfile modified

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/sathyasivamloganathan/portfolio",
+  base: "/portfolio/",
   plugins: [react()],
 });
+
